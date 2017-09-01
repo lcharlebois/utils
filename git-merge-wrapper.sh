a5eb55380419ce1b10e2bf1c3e017722cf8cd416
Leandre Charlebois <lcharlebois@sherweb.com>
2017-09-01 17:10:38 -0400

    Change file display name when merging

--- git-merge-wrapper.sh
+++ git-merge-wrapper.sh
@@ -3,5 +3,5 @@
 # Passing the following parameters to mergetool:
 #  local base remote merge_result
 
-#"C:/Program Files/SourceGear/Common/DiffMerge/sgdm.exe" "$1" "$2" "$3" --result="$4" --title1="Mine" --title2="Merging to: $4" --title3="Theirs"
-'C:/Program Files/KDiff3/kdiff3.exe' "$1" "$2" "$3" -o "$4" --auto --L1 "BASE" --L2 "Mine" --L3 "Theirs"
+#"C:/Program Files/SourceGear/Common/DiffMerge/sgdm.exe" "$1" "$2" "$3" --result="$4" --title1="Base" --title2="Mine"  --title3="Theirs"
+'C:/Program Files/KDiff3/kdiff3.exe' "$1" "$2" "$3" -o "$4" --auto --L1 "Base" --L2 "Mine" --L3 "Theirs"
