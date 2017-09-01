f6dea0b8a663139eedc232d23628efe5476f71d8
Leandre Charlebois <lcharlebois@sherweb.com>
2017-09-01 17:04:42 -0400

    Add script to set mergetool and difftool

---
+++ git-merge-wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Passing the following parameters to mergetool:
+#  local base remote merge_result
+
+#"C:/Program Files/SourceGear/Common/DiffMerge/sgdm.exe" "$1" "$2" "$3" --result="$4" --title1="Mine" --title2="Merging to: $4" --title3="Theirs"
+'C:/Program Files/KDiff3/kdiff3.exe' "$1" "$2" "$3" -o "$4" --auto
